440c0469d785b6ca98d8e212845bf8843bb6eb69
RedPaladin35 <ishans7305@gmail.com>
2026-01-24 10:08:12 +0530

    Add files via upload

---
+++ BDAT/week3/sample_ex/run_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+JAR_PATH="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.1.jar"
+
+INPUT_PATH="/user/230968078/week3/input.txt"
+OUTPUT_PATH="/user/230968078/week3/result"
+
+echo "Starting hadoop job..."
+hadoop jar $JAR_PATH \
+    -file mapper.py -mapper "python3 mapper.py" \
+    -file reducer.py -reducer "python3 reducer.py" \
+    -input $INPUT_PATH \
+    -output $OUTPUT_PATH
+
+echo "Job complete. Results:" 
+hdfs dfs -cat $OUTPUT_PATH/part*
